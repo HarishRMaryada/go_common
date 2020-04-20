b12c96927b7dd69bcbc607440834fc5deaec6e44
Harish <>
2020-04-20 22:13:55 +0530

    added routes and controller

---
+++ routes/index.go
@@ -0,0 +1,40 @@
+package routes
+
+import (
+	"go_common/controllers"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		controllers.Index,
+	},
+}
+
+func NewRouter() *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+
+	return router
+}
